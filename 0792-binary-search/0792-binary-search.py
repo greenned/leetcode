ef5d7bd7c0eb858f2ddb9d38036bc9619fbd7801
greenned <42107709+greenned@users.noreply.github.com>
2024-12-18 21:01:55 +0900

    Time: 3 ms (8.47%), Space: 18.9 MB (10.88%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,9 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        try:
+            index = nums.index(target)
+            return index
+        except:
+            return -1
+        
