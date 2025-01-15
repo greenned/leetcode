4d1ca7a4e049695dca3206804494e348650b4a89
greenned <42107709+greenned@users.noreply.github.com>
2025-01-15 19:13:30 +0900

    Time: 6 ms (92.61%), Space: 17.7 MB (51.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        # if sorted(s) == sorted(t):
+        #     return True
+        # else:
+        #     return False
+        # 17ms Beats 23.64%
         
+        s_map = {}
+        for ss in s:
+            s_map[ss] = s_map.get(ss, 0) + 1
+        
+        t_map = {}
+        for tt in t:
+            t_map[tt] = t_map.get(tt, 0) + 1
+        return s_map == t_map
